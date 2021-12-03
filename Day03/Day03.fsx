631d68be30058cfd7fe6a5274d4794f09cfde1fa
tom <leko.tomas@fronius.com>
2021-12-03 08:08:56 +0100

    refactoring

--- Day03/Day03.fsx
+++ Day03/Day03.fsx
@@ -7,19 +7,12 @@ let getPath fileName = __SOURCE_DIRECTORY__ </> fileName
 let read fileName =
     getPath fileName
     |> File.ReadAllLines
-    |> Array.map (Seq.map (string >> int) >> Array.ofSeq)
 
-let countOnes numbers =
-    let sumItem (x, y) = x + y
-    let sumRows row1 row2 =
-        Array.zip row1 row2
-        |> Array.map sumItem
+let countOnes (numbers: string[]) (pos: int) =
     numbers
-    |> Array.reduce sumRows
+    |> Array.sumBy (fun number -> if number.[pos] = '1' then 1 else 0)
 
-let bitsToNumber (bits: int[]) =
-    bits 
-    |> Array.fold (fun acc bit -> acc <<< 1 ||| bit) 0
+let toInt (number: string) = Convert.ToInt32(number, 2)
 
 let (|MostOnes|MostZeros|Equal|) (numbersCount, onesCount) =
     if 2*onesCount = numbersCount 
@@ -28,43 +21,38 @@ let (|MostOnes|MostZeros|Equal|) (numbersCount, onesCount) =
     then MostOnes
     else MostZeros
 
-let mostCommonBit numbersCount onesCount =
-    match numbersCount, onesCount with
-    | MostOnes -> 1
-    | MostZeros -> 0
+let mostCommonBit = function
+    | MostOnes -> '1'
+    | MostZeros -> '0'
     | Equal -> failwith "equal count of zeros and ones"
 
-let leastCommonBit numbersCount onesCount =
-    match numbersCount, onesCount with
-    | MostOnes -> 0
-    | MostZeros -> 1
+let leastCommonBit = function
+    | MostOnes -> '0'
+    | MostZeros -> '1'
     | Equal -> failwith "equal count of zeros and ones"
 
 let solve fileName =
     let numbers = read fileName
-    let onesCount = countOnes numbers
 
-    let toNumber predicate = 
-        onesCount 
-        |> Array.map predicate 
-        |> bitsToNumber
-    
-    let gammaRate = toNumber (mostCommonBit numbers.Length)
-    let epsilonRate = toNumber (leastCommonBit numbers.Length)
-    gammaRate * epsilonRate
+    let calcRate (getBit: (int*int -> char)) =
+        [| for bitPos in 0..numbers.[0].Length-1 do
+            getBit (numbers.Length, (countOnes numbers bitPos)) |]
+        |> String
+    ()
+
+    let gammaRate = calcRate mostCommonBit
+    let epsilonRate = calcRate leastCommonBit
+    (toInt gammaRate) * (toInt epsilonRate)
 
 solve "sample.txt" // 198
 solve "input.txt" // 2250414
 
-let someRating ratingFilter (numbers: int[][]) =
-    let filter bitPos bitValue (numbers: int[][]) =
+let someRating ratingFilter (numbers: string[]) =
+    let filter bitPos (bitValue: char) (numbers: string[]) =
         numbers
         |> Array.filter (fun number -> number.[bitPos] = bitValue)
-
-    let rec loop numbers bitPos =
-        let onesCount = countOnes numbers
-        let bitValue = ratingFilter (numbers.Length, onesCount.[bitPos])
-
+    let rec loop (numbers: string[]) (bitPos: int) =
+        let bitValue = ratingFilter (numbers.Length, countOnes numbers bitPos)
         let numbers' = filter bitPos bitValue numbers
         if numbers'.Length = 1 
         then numbers'.[0]
@@ -72,25 +60,25 @@ let someRating ratingFilter (numbers: int[][]) =
 
     loop numbers 0
 
-let oxygenGeneratorRating (numbers: int[][]) =
+let oxygenGeneratorRating (numbers: string[]) =
     let getFilterValue = function
-        | MostOnes -> 1
-        | MostZeros -> 0
-        | Equal -> 1
+        | MostOnes -> '1'
+        | MostZeros -> '0'
+        | Equal -> '1'
     someRating getFilterValue numbers
 
-let co2ScrubberRating (numbers: int[][]) =
+let co2ScrubberRating (numbers: string[]) =
     let getFilterValue = function
-        | MostOnes -> 0
-        | MostZeros -> 1
-        | Equal -> 0
+        | MostOnes -> '0'
+        | MostZeros -> '1'
+        | Equal -> '0'
     someRating getFilterValue numbers
 
 let solve2 fileName =
     let numbers = read fileName
     let ogr = oxygenGeneratorRating numbers
     let csr = co2ScrubberRating numbers
-    (bitsToNumber ogr) * (bitsToNumber csr)
+    (toInt ogr) * (toInt csr)
 
-solve2 "sample.txt"
-solve2 "input.txt"
+solve2 "sample.txt" // 230
+solve2 "input.txt" // 6085575
