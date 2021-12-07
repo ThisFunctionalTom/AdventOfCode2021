2b52ca60d78fd2a5c35f696c7a937e0b4eef5c99
Tomas Leko <tomas.leko@gmail.com>
2021-12-07 06:54:49 +0100

    reafactoring Day07

--- Day07/Day07.fsx
+++ Day07/Day07.fsx
@@ -4,25 +4,28 @@ let (</>) p1 p2 = Path.Combine (p1, p2)
 let getPath fileName = __SOURCE_DIRECTORY__ </> fileName
 
 let read (fileName: string) = 
-    let input = getPath fileName |> File.ReadAllText 
-    input.Split "," |> Array.map int
+    let input = 
+        getPath fileName
+        |> File.ReadAllText
+    
+    input.Split "," 
+    |> Array.map int
 
-let getFuel1 pos crabs =
-    crabs |> Array.sumBy (fun cp -> abs (cp - pos))
+let getFuel1 pos crabPos =
+    abs (crabPos - pos)
 
-let getFuel2 pos crabs =
-    crabs 
-    |> Array.sumBy (fun cp -> 
-        let n = abs (cp - pos)
-        (n * (n+1)) / 2)
+let getFuel2 pos crabPos =
+    let n = abs (crabPos - pos)
+    (n * (n+1)) / 2 // Array.sumBy [0..n]
 
-let solve (getFuel: int -> int[] -> int) fileName =
+let solve (getFuel: int -> int -> int) fileName =
     let crabs = read fileName
     let p1 = Array.min crabs
     let p2 = Array.max crabs
 
     [ for p in p1 .. p2 do
-        getFuel p crabs ] |> List.min
+        crabs |> Array.sumBy (getFuel p) ] 
+    |> List.min
 
 solve getFuel1 "sample.txt" // 37
 solve getFuel1 "input.txt" // 355592
