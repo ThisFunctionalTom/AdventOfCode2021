b2fe536bcce43a042e9330f962fbe907a6e08bca
Tomas Leko <tomas.leko@gmail.com>
2021-12-07 09:16:26 +0100

    Day 07 median, mean

--- Day07/Day07.fsx
+++ Day07/Day07.fsx
@@ -18,17 +18,56 @@ let getFuel2 pos crabPos =
     let n = abs (crabPos - pos)
     (n * (n+1)) / 2 // Array.sumBy [0..n]
 
+let getTotalFuel (getCrabFuel: int -> int) (crabs: int[]) =
+    crabs |> Array.sumBy getCrabFuel
+
 let solve (getFuel: int -> int -> int) fileName =
     let crabs = read fileName
     let p1 = Array.min crabs
     let p2 = Array.max crabs
 
-    [ for p in p1 .. p2 do
-        crabs |> Array.sumBy (getFuel p) ] 
-    |> List.min
+    List.min [ for p in p1 .. p2 do getTotalFuel (getFuel p) crabs ] 
 
 solve getFuel1 "sample.txt" // 37
 solve getFuel1 "input.txt" // 355592
 
 solve getFuel2 "sample.txt" // 168
 solve getFuel2 "input.txt" // 101618069 
+
+let crabs = read "input.txt"
+let crabs = read "sample.txt"
+
+module Array =
+    let median arr = Array.sort arr |> Array.item (Array.length arr / 2)
+    let mean arr = Array.sum arr / Array.length arr
+
+let findMinFuel getStart getFuel crabs =
+    let getTotalFor p =
+        getTotalFuel (getFuel p) crabs, p
+    
+    let rec loop count (tf, p) =
+        let (tf', p') = min (getTotalFor (p+1)) (getTotalFor (p-1))
+        ()
+        if tf' < tf 
+        then loop (count+1) (tf', p') 
+        else tf, p, count
+    getStart crabs
+    |> getTotalFor
+    |> loop 0
+
+[ for file in [ "sample.txt", "input.txt" ] do
+    file, findMinFuel Array.median getFuel1 (read file)
+    file, findMinFuel Array.mean getFuel2 (read file) ]
+
+let median = Array.sort crabs |> Array.item (Array.length crabs / 2)
+
+let smaller, bigger = crabs |> Array.partition (fun cp -> cp < mean)
+
+[ for p in Array.max smaller .. Array.min bigger do
+    getTotalFuel (getFuel2 p) crabs ]
+
+smaller |> Array.sumBy (getFuel2 347)
+smaller |> Array.sumBy (getFuel2 349)
+bigger |> Array.sumBy (getFuel2 347)
+bigger |> Array.sumBy (getFuel2 349)
+
