ca1daf043bf24cb03d1adea22cdf5b30daaebd27
Tomas Leko <tomas.leko@gmail.com>
2021-12-12 10:01:23 +0100

    Day12 part 2

--- Day12/Day12.fs
+++ Day12/Day12.fs
@@ -18,6 +18,13 @@ let read fileName : Map<Cave, Cave list> =
     |> Array.map (fun (fromCave, lst) -> fromCave, lst |> Array.map snd |> Array.toList)
     |> Map.ofArray
 
+let (|Start|End|Small|Big|) =
+    function
+    | "start" -> Start
+    | "end" -> End
+    | cave when cave.ToLowerInvariant () = cave -> Small
+    | _ -> Big
+
 module Path =
     let isComplete =
         function
@@ -26,26 +33,58 @@ module Path =
 
     let show = List.rev >> String.concat "->"
 
+    let isValid1 (path : Path) =
+        path
+        |> List.countBy id
+        |> List.forall (
+            function
+            | Start, cnt -> cnt = 1
+            | End, cnt -> cnt = 1
+            | Small, cnt -> cnt = 1
+            | Big, _ -> true
+        )
+
+    let isValid2 (path : Path) =
+        let caveCounts = path |> List.countBy id
+
+        let isSmall =
+            function
+            | Small -> true
+            | _ -> false
+
+        let smallCaveCounts =
+            caveCounts |> List.filter (fst >> isSmall)
+
+        let caveCountsOk =
+            caveCounts
+            |> List.forall (
+                function
+                | Start, cnt -> cnt = 1
+                | End, cnt -> cnt = 1
+                | Small, cnt -> cnt <= 2
+                | Big, _ -> true
+            )
+
+        let smallCaveCountsOk =
+            (smallCaveCounts |> List.sumBy snd)
+            <= smallCaveCounts.Length + 1
+
+        caveCountsOk && smallCaveCountsOk
+
 #if INTERACTIVE
 fsi.AddPrinter Path.show
 #endif
 
-let step (map : Map<Cave, Cave list>) (path : Path) =
+let step isPathValid (map : Map<Cave, Cave list>) (path : Path) =
     match path with
     | [] -> failwith "Should never happen"
     | cave :: _ ->
         map
         |> Map.find cave
-        |> List.filter (fun c ->
-            let doubleVisit =
-                c.ToLowerInvariant () = c
-                && path |> List.contains c
-
-            not doubleVisit
-        )
         |> List.map (fun c -> c :: path)
+        |> List.filter isPathValid
 
-let solve fileName =
+let solve step fileName =
     let map = read fileName
 
     let rec loop complete incomplete =
@@ -64,7 +103,16 @@ let solve fileName =
     loop Set.empty (Set.singleton [ "start" ])
     |> Set.count
 
-solve "sample.txt" |> printfn "%d" // 10
-solve "sample2.txt" |> printfn "%d" // 19
-solve "sample3.txt" |> printfn "%d" // 226
-solve "input.txt" |> printfn "%d" // 5212
+let solve1 = solve (step Path.isValid1)
+
+solve1 "sample.txt" |> printfn "%d" // 10
+solve1 "sample2.txt" |> printfn "%d" // 19
+solve1 "sample3.txt" |> printfn "%d" // 226
+solve1 "input.txt" |> printfn "%d" // 5212
+
+let solve2 = solve (step Path.isValid2)
+
+solve2 "sample.txt" |> printfn "%d" // 36
+solve2 "sample2.txt" |> printfn "%d" // 103
+solve2 "sample3.txt" |> printfn "%d" // 3509
+solve2 "input.txt" |> printfn "%d" // 134862
